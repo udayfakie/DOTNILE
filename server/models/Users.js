7a95f2a13ac92e61ec30653e705b13de3722606c
udayfakie <92738263+udayfakie@users.noreply.github.com>
2023-03-20 01:04:46 +0200

    'post-req'

--- server/models/Users.js
+++ server/models/Users.js
@@ -1,15 +1,15 @@
-const mongoose = require('mongoose')
+const { Schema, model } = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-    name:{
-        type: String,
-    },
-    age:{
-        type: Number,
-    },
-    email:{
-        type: String,
-    },
-})
-const UserModel =  mongoose.model("users",UserSchema)
-module.exports = UserModel
+const UserSchema = new Schema({
+  name: {
+    type: String,
+  },
+  age: {
+    type: Number,
+  },
+  email: {
+    type: String,
+  },
+});
+const UserModel = model("users", UserSchema);
+module.exports = UserModel;
--- server/server.js
+++ server/server.js
@@ -7,13 +7,24 @@ const UserModel = require("./models/Users");
 dotenv.config();
 const app = express();
 const PORT = 3001;
-app.use(cors())
+app.use(cors());
+app.use(express.json());
 
+//CONNECT TO THE DATA_BASE
 mongoose.connect(process.env.DATABASE_CONNETCTION);
-
+//GET REQUEST
 app.get("/users", async (req, res) => {
   const users = await UserModel.find();
   res.json(users);
+});
+
+//CREATE USEERS
+app.post("/createUser", async (req, res) => {
+  const user = req.body;
+  const newUser = new UserModel(req.body);
+  await newUser.save();
+
+  res.json(user);
 });
 
 app.listen(PORT, () => {
