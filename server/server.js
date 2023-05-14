d6576d12e0cca9cf96b6c9c1115a63fb6a7ba5df
udayfakie <92738263+udayfakie@users.noreply.github.com>
2023-05-14 22:34:06 +0300

    node engine specified

--- server/server.js
+++ server/server.js
@@ -10,6 +10,23 @@ const PORT = 3001;
 app.use(cors());
 app.use(express.json());
 
+// Set the path to the static directory
+// const staticPath = path.join(__dirname, "public");
+
+// Set the path to the public directory
+// const publicPath = path.join(__dirname, "public");
+
+// Serve static files from the static directory
+// app.use(express.static(staticPath));
+
+// Serve public files from the public directory
+// app.use(express.static(publicPath));
+
+// Route all other requests to the index.html file
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public", "index.html"));
+// });
+
 //CONNECT TO THE DATA_BASE
 mongoose.connect(process.env.DATABASE_CONNETCTION);
 //GET REQUEST
@@ -33,6 +50,24 @@ app.delete("/:id", async (req, res) => {
       return res.status(404).json({ msg: "User not found" });
     }
     res.json(deletedUser);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+app.patch("/:id", async (req, res) => {
+  try {
+    const { name, age, email } = req.body;
+    const updatedUser = await UserModel.findByIdAndUpdate(
+      req.params.id,
+      { $set: { name, age, email } },
+      { new: true }
+    );
+    if (!updatedUser) {
+      return res.status(404).json({ msg: "User not found" });
+    }
+    res.json(updatedUser);
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server Error");
