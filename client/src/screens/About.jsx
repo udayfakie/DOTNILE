3bef6e63b6662f5f0a69b9333b649b30c1b87118
Goanar Abraham <goanarabraham@Goanars-MacBook-Pro.local>
2023-06-20 22:38:14 +0300

    about page added

--- client/src/screens/About.jsx
+++ client/src/screens/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div>
       <h2>About Us</h2>
-      <p>Welcome to our website! We are a company dedicated to providing top-notch services and products.</p>
+      <p>Welcome to our  world website! We are a company dedicated to providing top-notch services and products.</p>
       <p>Feel free to explore our offerings and reach out to us if you have any questions or inquiries.</p>
     </div>
   );
